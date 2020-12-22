247ee0f03c70eeca09beff36edd87f5a6751bc8c
shunnarski <ashunnarah9597@gmail.com>
2020-12-21 18:20:55 -0600

    Seeing if env variables are loading in node script

--- server.js
+++ server.js
@@ -256,6 +256,7 @@ app.post('/deleteGiftFromList', function(req, res) {
 // starting server on port 8080
 const port = process.env.port || 8081;
 app.listen(port, () => {
+    console.log(proces.env);
     console.log("Server started!");
     console.log("on port " + port);
 })
